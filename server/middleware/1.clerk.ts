fe1064ae45d4384f7211748bc5a6ac1756f98679
TusharW4ni <reachtusharwani@gmail.com>
2025-04-03 23:01:27 -0500

    ommited _hub routes from auth

--- server/middleware/1.clerk.ts
+++ server/middleware/1.clerk.ts
@@ -4,7 +4,7 @@ export default clerkMiddleware(async (event) => {
   const { userId } = event.context.auth();
   const runtimeConfig = useRuntimeConfig();
   console.log({ runtimeConfig: runtimeConfig.PROD_CLERK_REDIRECT_URL });
-  if (!userId) {
+  if (!userId && !event.path.includes("_hub")) {
     await sendRedirect(
       event,
       `https://accounts.tusharwani.com/sign-in?redirect_url=${encodeURIComponent(
